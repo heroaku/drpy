600fd37440187408f190cd30b2957af3fad2cb63
hjdhnx <hjd124579>
2022-11-20 14:37:48 +0800

    更新了

--- jiexi/刚刚.js
+++ jiexi/刚刚.js
@@ -1,1 +1,2 @@
-realUrl = 重定向('https://play.fuqizhishi.com/juhe/API.php?appkey=caijijuhe220902&url='+vipUrl)
+// realUrl = 重定向('https://play.fuqizhishi.com/juhe/API.php?appkey=caijijuhe220902&url='+vipUrl)
+realUrl = 重定向('http://jxhc.ioivip.xyz/?url='+vipUrl)
