921aeb668b1d32078d1148b81d748a6a189df4ed
tloook <tlooo.cute@gmail.com>
2022-12-20 14:35:34 +0800

    更新源

--- js/555影视.js
+++ js/555影视.js
@@ -1,11 +1,14 @@
-var rule = Object.assign(muban.mxpro,{
-title:'555影视',
-// host:'https://www.555dy.app',
-// host:'https://www.555yy2.com/',
-// host:'https://www.555yy1.com/',
-host:'https://www.555pian.com',
-headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-    'User-Agent':'MOBILE_UA',
-    "Cookie": "searchneed=ok"
-},
-});
+var rule = {
+    title:'555影视',
+    模板:'mxpro',
+    // host:'https://www.555dy.app',
+    // host:'https://www.555yy2.com',
+    // host:'https://www.555ys1.com',
+    host:'https://www.555pian.com',
+    url:'/vodshow/fyclass--------fypage---/pjax/YES.html',
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'PC_UA',
+        "Cookie": "searchneed=ok",
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    },
+}
